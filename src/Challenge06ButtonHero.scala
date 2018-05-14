75d9d107456a0be03c7cdd2d33c3225e0877bcb5
Ramón Gil Moreno <ramongilmoreno@gmail.com>
2018-05-14 10:04:59 +0200

    Yet very slow optimization of Button Hero that handles submit cases

--- src/Challenge06ButtonHero.scala
+++ src/Challenge06ButtonHero.scala
@@ -22,32 +22,57 @@ object Challenge06ButtonHero {
   }
 
   trait Status { def down: Float; def score: Int }
-  case class Down (down: Float, score: Int) extends Status
+  case class Down (down: Float, score: Int, pending: Set[Note]) extends Status
   case class Up (down: Float, up: Float, score: Int) extends Status
 
   def start (note: Note): Float = note.start.toFloat / note.speed
   def end (note: Note): Float = (note.start + note.length).toFloat / note.speed
 
-  def solve(statuses: Set[Status], events: mutable.SortedSet[Event]): Set[Status] = {
+  def solve(lastTime: Float, statuses: Set[Status], events: mutable.SortedSet[Event]): Set[Status] = {
+    // Filter empty downs and old ups
+    var (filteredDown, filteredUp) = statuses.partition(_.isInstanceOf[Down])
+    filteredDown = filteredDown.filter(_ match {
+      case s: Down if s.pending.isEmpty => false
+      case _ => true
+    })
+    var (lastUps, oldUps) = filteredUp.partition(_ match {
+      // Filter empty up
+      case s: Up if (s.up == lastTime) => true
+      case _ => false
+    })
+    if (oldUps.nonEmpty) {
+      oldUps = Set(oldUps.maxBy(_.score))
+    }
+
+    // Proceed
+    val statuses2 = filteredDown ++ (lastUps ++ oldUps)
     if (events.isEmpty) {
-      statuses
+      statuses2
     } else {
       val event = events.head
-      solve(
+      solve(event.time,
         if (event.start) {
-          var (downs, ups) = statuses.partition(_.isInstanceOf[Down])
+          // In the case of start, either join other button down at the same time
+          // or convert previous button up in down
+          var (downs, ups) = statuses2.partition(_.isInstanceOf[Down])
           if (!downs.exists(_.down == start(event.note))) {
-            downs = downs + Down(event.time, 0)
+            downs = downs + Down(event.time, 0, Set(event.note))
+          } else {
+            downs = downs.map(_ match {
+              case x: Down if (x.down == event.start) => Down(x.down, x.score, x.pending + event.note)
+              case x => x
+            })
           }
+
           ups = ups.flatMap(_ match {
-            case x: Up if (x.up < event.time) => Set[Status](x, Down(event.time, x.score))
+            case x: Up if (x.up < event.time) => Set[Status](x, Down(event.time, x.score, Set(event.note)))
             case x => Set[Status](x)
           })
           ups ++ downs
         } else {
           // Collect points on all started status
-          statuses.flatMap(_ match {
-            case s: Down if (s.down == start(event.note)) => Set[Status](s, Up(s.down, event.time, s.score + event.note.points))
+          statuses2.flatMap(_ match {
+            case s: Down if (s.down == start(event.note)) => Set[Status](Down(s.down, s.score, s.pending - event.note), Up(s.down, event.time, s.score + event.note.points))
             case s: Up if ((s.down == start(event.note)) && (s.up == end(event.note))) => Set[Status](Up(s.down, s.up, s.score + event.note.points))
             case s => Set(s)
           })
@@ -67,7 +92,7 @@ object Challenge06ButtonHero {
         events += Event(note, false, end(note))
         scanner.nextLine
       }
-      val solution = solve(Set(Up(-1, -1, 0)), events).map(_.score).max
+      val solution = solve(-1, Set(Up(-1, -1, 0)), events).map(_.score).max
       println(s"Case #${i}: ${solution}")
     }
   }
